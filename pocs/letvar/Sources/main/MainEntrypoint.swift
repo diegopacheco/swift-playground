7618c91ff528470f4357bc00e1ba8f23f104e8e8
diegopacheco <diego.pacheco.it@gmail.com>
2026-01-01 14:51:05 -0800

    added let and var

---
+++ pocs/letvar/Sources/main/MainEntrypoint.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct main {
+    static func main() {
+        let x = 42
+        var y = 0
+        print("x = \(x), y = \(y)")
+    }
+}
