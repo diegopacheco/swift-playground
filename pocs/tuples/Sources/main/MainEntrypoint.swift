91682348188d4375e3b4ee20662b56dcca17d844
diegopacheco <diego.pacheco.it@gmail.com>
2026-01-02 01:17:04 -0800

    added tuples

---
+++ pocs/tuples/Sources/main/MainEntrypoint.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+@main
+struct main {
+    static func main() {
+        let http404Error = (404, "Not Found")
+        print("The status code is \(http404Error.0)")
+    }
+}
